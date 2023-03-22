9c694281ce327aa987365ce87fb88250536c3b3e
WonderSlippers <74339322+WonderSlippers@users.noreply.github.com>
2023-03-22 16:55:17 +0800

    增加了多个boot

---
+++ all/Commodity/src/main/java/com/shop/CommodityStarter.java
@@ -0,0 +1,13 @@
+package com.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommodityStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommodityStarter.class);
+    }
+
+}
---
+++ all/Message/src/main/java/com/shop/MessageStarter.java
@@ -0,0 +1,13 @@
+package com.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageStarter.class);
+    }
+
+}
